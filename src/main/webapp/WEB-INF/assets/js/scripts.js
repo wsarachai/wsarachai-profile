4f37284a17f39a90cc234a0e6d902778a8e28f1d
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-07-13 18:17:19 +0700

    new style for home page

---
+++ src/main/webapp/WEB-INF/assets/js/scripts.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const filterCourses = function() {
+    let input, filter, courseContainer, courseItems, courseHeader, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    courseContainer = document.querySelector('.course-container .row');
+    courseItems = courseContainer.getElementsByClassName('course-item');
+
+    for (i = 0; i < courseItems.length; i++) {
+        courseHeader = courseItems[i].getElementsByClassName('course-header')[0];
+        txtValue = courseHeader.textContent || courseHeader.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            courseItems[i].style.display = "";
+        } else {
+            courseItems[i].style.display = "none";
+        }
+    }
+}

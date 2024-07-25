100470dae5eed9bf974b5eebd83d3f4281d7466a
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-07-25 10:17:03 +0700

    clean linebot lib

--- src/main/java/org/itsci/config/MyChannelTokenSupplier.java
+++
@@ -1,9 +0,0 @@
-package org.itsci.config;
-
-import com.linecorp.bot.client.ChannelTokenSupplier;
-
-public class MyChannelTokenSupplier implements ChannelTokenSupplier {
-    public String get() {
-        return "";
-    }
-}
